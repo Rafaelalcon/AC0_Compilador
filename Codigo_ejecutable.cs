59a49a1da193d23957a876d1d661010d3476e488
Rafaelalcon <118633621+Rafaelalcon@users.noreply.github.com>
2022-11-19 21:27:32 +0100

    Add files via upload

---
+++ Codigo_ejecutable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Codigo_ejecutable
+{
+    class Codigo_ejecutable
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, total;
+
+            Console.WriteLine("Dona'm el primer valor: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dona'm el segon valor: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            total = number1 + number2;
+            Console.WriteLine("la suma correcta és: " + total);
+            Console.ReadLine();
+        }
+    }
+}
